4b7f39d2e98a08a943fcb4544e3629800e94f065
Alan-Liang <yalun_liang@keeer.ga>
2019-05-12 14:09:59 +0800

    feat(server): Form utils

--- src/form.js
+++ src/form.js
@@ -3,16 +3,14 @@ import Question from './question'
 import Plugin from './plugin'
 import Theme from './theme'
 // TODO
-/**
- * Class representing a page.
- */
-class Page {
+/** Class representing a page. */
+export class Page {
   /**
    * Creates a page object.
    * @param {Object} options Options, see below.
    * @param {string} options.title The title of the page
    * @param {string|number} options.id Page ID
-   * @param {module:question~Question} options.questions[] questions in the page.
+   * @param {module:question~Question[]} options.questions Questions in the page
    */
   constructor(options) {
     this.is = 'Page'
@@ -27,7 +25,8 @@ class Page {
   }
 
   /**
-   * TODO
+   * Get a object to be stored.
+   * @returns {object} Object representing the page
    */
   toObject() {
     return {
@@ -36,4 +35,39 @@ class Page {
       questions: this.questions.map(q => q.id)
     }
   }
+}
+
+/** Class representing a form. */
+export class Form {
+  /**
+   * Creates a form object.
+   * @param {object} options Options, see below.
+   * @param {string} options.title The title of the form
+   * @param {string|number} [options.id] Form ID
+   * @param {Page[]} options.pages Pages in the form
+   */
+  constructor(options) {
+    const {
+      id,
+      title,
+      pages,
+    } = options
+    this.id = id
+    this.title = title
+    this.pages = pages
+
+    this.data = options
+  }
+
+  /**
+   * Gets the questions in the form.
+   * @returns {module:question~Question[]} Questions.
+   */
+  get questions() {
+    return this.pages.flatMap(p => p.questions)
+  }
+
+  // TODO: DB
+  static async fromId() {}
+  async save() {}
 }
