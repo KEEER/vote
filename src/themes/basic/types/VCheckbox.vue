8adf8afc84f20d1628215fc93351cec19ae040dc
Alan-Liang <a@keeer.net>
2020-02-17 13:49:38 +0800

    fix(theme-basic): VCheckbox unable to sync value

--- src/themes/basic/types/VCheckbox.vue
+++ src/themes/basic/types/VCheckbox.vue
@@ -5,13 +5,19 @@
       :multiline="multiline"
       :key="option.value"
       :option="option"
-      :value.sync="value_[option.value]">
-    </VCheckboxInput>
+      :value.sync="value_[option.value]"
+      @update:value="syncValue"
+    />
   </div>
 </template>
 
 <script>
 import VCheckboxInput from './VCheckboxInput'
+import hooks from '../hooks'
+
+hooks.on('question:validate', ([ q, s ]) => {
+  if (q.type === 'VCheckbox' && q.data.required && Object.values(q.value || {}).every(v => !v)) s(false)
+})
 
 export default {
   name: 'VCheckbox',
@@ -35,6 +41,11 @@ export default {
     multiline () {
       if (!this.data.config || !this.data.config.theme) return false
       return this.data.config.theme.multiline
+    },
+  },
+  methods: {
+    syncValue () {
+      this.$emit('update:value', { ...this.value_ })
     },
   },
 }
