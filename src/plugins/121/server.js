7d5f7adb77b39d881ad053cbe20fd3f3eba93ac4
Alan-Liang <a@keeer.net>
2020-08-31 07:26:31 +0800

    fix(plugin-121): multiple query

--- src/plugins/121/server.js
+++ src/plugins/121/server.js
@@ -14,48 +14,51 @@ export default form => {
     if (!me) return set(ctx.$t('plugin.121.noMe'))
     const youIds = map.filter(m => m[me.type] === me.id)
     if (!youIds) return set(ctx.$t('plugin.121.noYou'))
-    const yous = youIds.map(youId => people.find(p => p.id === youId[me.type === 'junior' ? 'senior' : 'junior']))
-    if (!yous) return set('内部错误 2')
 
-    let str = ""
-    for ([you, youId] of [yous, youIds]) {
-      const pronoun = { male: '他', female: '她' }[you.gender]
-      const escaper = (strings, ...keys) => {
-        let html = ''
-        for (const i of keys.keys()) {
-          html += strings[i]
-          html += keys[i].replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/\n/g, '<br>').replace(/&lt;strong&gt;/g, '<strong>').replace(/&lt;\/strong&gt;/g, '</strong>')
-        }
-        html += strings[strings.length - 1]
-        return html
+    const strong = x => `<strong>${x}</strong>`
+    const html = []
+    const escaped = (strings, ...keys) => {
+      let html = ''
+      for (const i of keys.keys()) {
+        html += strings[i]
+        html += keys[i].replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/\n/g, '<br>').replace(/&lt;strong&gt;/g, '<strong>').replace(/&lt;\/strong&gt;/g, '</strong>')
       }
-      const strong = x => `<strong>${x}</strong>`
-      let html = []
-      html.push(escaper`
-        <p>${me.name}，你好！</p>
+      html += strings[strings.length - 1]
+      return html
+    }
+    html.push(escaped`<p>${me.name}，你好！</p>`)
+    for (const youId of youIds) {
+      const you = people.find(p => p.id === youId[me.type === 'junior' ? 'senior' : 'junior'])
+      if (!you) return set('内部错误 2')
+      const pronoun = { male: '他', female: '她' }[you.gender]
+      const processHtml = (...args) => escaped(...args).replace(/__pronoun/g, pronoun)
+      html.push(processHtml`
         <p>
           你匹配到的${you.type === 'senior' ? '大朋友' : '小朋友'}是：<strong>${you.name}</strong>。恭喜你！
           __pronoun的手机号是 <strong>${you.number}</strong>，微信号是 <strong>${you.微信号}</strong>，名称为「<span dir="ltr">${you.微信名}</span>」，你可以添加__pronoun的微信来和__pronoun联系。
         </p>
       `)
-      if (you.年级 === 'ICC') html.push(escaper`
+      if (you.年级 === 'junior') html.push(processHtml`
         <p>
-          ${you.name}初中${you.人本 ? '在' : '不在'}人本就读，
-          ${you.住宿生}住宿生，
+          __pronoun${you.住宿生}住宿生，
           最常用的软件有 ${you.软件.map(strong).join('、')}。
         </p>
       `)
-      else html.push('<p>TODO</p>')
-
-      str += html.join('\n').replace(/\n\s*|\n\s*\n/g, '\n').replace(/__pronoun/g, pronoun)
+      else html.push(processHtml`
+      <p>
+        ${you.name}初中${you.人本 ? '在' : '不在'}人本就读，
+        ${you.住宿生}住宿生，
+        最常用的软件有 ${you.软件.map(strong).join('、')}。
+      </p>
+    `)
     }
 
-    str += escaper`
+    html.push(`
       <p>祝你们的交流顺利、愉快！</p>
       <p>人大附中志愿团</p>
-    `
+    `)
 
-    return set(str)
+    return set(html.join('\n').replace(/\n\s*|\n\s*\n/g, '\n'))
   })
   form.on('bundle', ({ data }) => getConfig(data, 'settings', settingsKey) && setConfig(data, 'settings', settingsKey, null))
 }
